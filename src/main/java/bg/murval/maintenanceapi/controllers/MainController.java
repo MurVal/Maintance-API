346cbeaf6b47f534c38288eeb6874b978d15863a
Cal1br <atillamexmed3@gmail.com>
2021-03-24 23:52:12 +0200

    optimized imports and added package controllers

--- src/main/java/bg/murval/maintenanceapi/controllers/MainController.java
+++ src/main/java/bg/murval/maintenanceapi/controllers/MainController.java
@@ -1,12 +1,18 @@
-package bg.murval.maintenanceapi;
+package bg.murval.maintenanceapi.controllers;
 
 import bg.murval.maintenanceapi.models.Task;
 import bg.murval.maintenanceapi.models.TaskRequest;
 import bg.murval.maintenanceapi.services.TaskService;
 import bg.murval.maintenanceapi.utils.Status;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 import java.util.Optional;
--- src/main/java/bg/murval/maintenanceapi/dao/InMemoryDao.java
+++ src/main/java/bg/murval/maintenanceapi/dao/InMemoryDao.java
@@ -3,7 +3,11 @@ package bg.murval.maintenanceapi.dao;
 import bg.murval.maintenanceapi.models.Task;
 import org.springframework.stereotype.Repository;
 
-import java.util.*;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Optional;
 import java.util.concurrent.atomic.AtomicLong;
 
 @Repository
